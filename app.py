b778aeac44ac9ff884a5fa2d96add526e5bccda2
cindywmiao <cindywmiao@gmail.com>
2017-02-06 14:54:10 +0800

    add timeline page

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/demo')
+def demo():
+    return render_template('demo.html')
+
 
 if __name__ == '__main__':
     app.run()
---
+++ static/js/main/main.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Cindy.Wang on 2/5/17.
+ */
+
+var myApp = angular.module('mainApp',[]);
+
+myApp.controller('mainController', ['$scope', function ($scope) {
+
+}]);
