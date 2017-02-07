0bb38c3d97e3471a23a019289f6b7046670249cd
cindywmiao <cindywmiao@gmail.com>
2017-02-07 17:46:15 +0800

    init hue page

---
+++ hue.py
@@ -0,0 +1,7 @@
+import utils.hdfs_util as hdfs_util
+
+def main():
+    print('Main')
+
+if __name__ == '__main__':
+    main()
--- stars.py
+++ stars.py
@@ -1,13 +1,35 @@
 from skyfield.api import load
 
 planets = load('de421.bsp')
-earth, mars = planets['earth'], planets['mars']
+
+# for i in planets:
+#     print(i)
+
+solar_system_barycenter = {
+    'MERCURY BARYCENTER'
+    , 'VENUS BARYCENTER'
+    , 'EARTH BARYCENTER'
+    , 'MARS BARYCENTER'
+    , 'JUPITER BARYCENTER'
+    , 'SATURN BARYCENTER'
+    , 'URANUS BARYCENTER'
+    , 'NEPTUNE BARYCENTER'
+    , 'PLUTO BARYCENTER'
+    , 'SUN'}
 
 ts = load.timescale()
 t = ts.now()
-position = earth.at(t).observe(mars)
-ra, dec, distance = position.radec()
+earth = planets['earth']
+for i in solar_system_barycenter:
+    if i is not 'earth':
+        planet = planets[i]
+        position = earth.at(t).observe(planet)
+        ra, dec, distance = position.radec()
 
-print(ra)
-print(dec)
-print(distance)
+        print(ra)
+        print(dec)
+        print(distance)
+
+def display_solar_system():
+
+
